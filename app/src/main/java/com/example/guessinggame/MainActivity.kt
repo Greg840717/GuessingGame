ed4d943ddddd62c84ea03ad7c57d1f573ab69823
Greg840717 <asdorm97531@gmail.com>
2019-11-23 11:48:25 +0800

    finish the judge function.

--- app/src/main/java/com/example/guessinggame/MainActivity.kt
+++ app/src/main/java/com/example/guessinggame/MainActivity.kt
@@ -2,24 +2,27 @@ package com.example.guessinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.nio.file.Files.find
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var playerName :EditText
-    private lateinit var status :TextView
-    private lateinit var scissors :RadioButton
-    private lateinit var stone :RadioButton
-    private lateinit var paper:RadioButton
-    private lateinit var start :Button
-    private lateinit var player :TextView
-    private lateinit var winner :TextView
-    private lateinit var mine :TextView
-    private lateinit var computer :TextView
+    private lateinit var playerName: EditText
+    private lateinit var status: TextView
+    private lateinit var scissors: RadioButton
+    private lateinit var stone: RadioButton
+    private lateinit var paper: RadioButton
+    private lateinit var start: Button
+    private lateinit var player: TextView
+    private lateinit var winner: TextView
+    private lateinit var mine: TextView
+    private lateinit var computer: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +39,22 @@ class MainActivity : AppCompatActivity() {
         computer = findViewById(R.id.Computer)
 
         initialLayoutText()
+        start.setOnClickListener {
+            if (playerName.length() < 1) {
+                status.setText("請輸入玩家姓名")
+            } else {
+                val computerGuess = (Math.random() * 3).toInt()
+                var playerGuess = 0
+                if (scissors.isChecked) {
+                    playerGuess = 0
+                } else if (stone.isChecked) {
+                    playerGuess = 1
+                } else {
+                    playerGuess = 2
+                }
+                winOrLose(playerGuess, computerGuess)
+            }
+        }
     }
 
     fun initialLayoutText() {
@@ -50,4 +69,59 @@ class MainActivity : AppCompatActivity() {
         mine.setText("玩家出拳")
         computer.setText("電腦出拳")
     }
+
+    fun winOrLose(a: Int, b: Int) {
+        player.setText("名子\n"+PlayerName.text.toString())
+        if (a == 0) {
+            when (b) {
+                0 -> {
+                    winner.setText("贏家\n平手")
+                    computer.setText("電腦出拳\n剪刀")
+                }
+                1 -> {
+                    winner.setText("贏家\n電腦")
+                    computer.setText("電腦出拳\n石頭")
+                }
+                2 -> {
+                    winner.setText("贏家\n" + PlayerName.text.toString())
+                    computer.setText("電腦出拳\n布")
+                }
+            }
+            mine.setText("玩家出拳\n剪刀")
+        } else if (a == 1) {
+            when (b) {
+                0 -> {
+                    winner.setText("贏家\n" + PlayerName.text.toString())
+                    computer.setText("電腦出拳\n剪刀")
+                }
+                1 -> {
+                    winner.setText("贏家\n平手")
+                    computer.setText("電腦出拳\n石頭")
+                }
+                2 -> {
+                    winner.setText("贏家\n電腦")
+                    computer.setText("電腦出拳\n布")
+                }
+            }
+            mine.setText("玩家出拳\n石頭")
+        } else {
+            when (b) {
+                0 -> {
+                    winner.setText("贏家\n電腦")
+                    computer.setText("電腦出拳\n剪刀")
+                }
+                1 -> {
+                    winner.setText("贏家\n" + PlayerName.text.toString())
+                    computer.setText("電腦出拳\n石頭")
+                }
+                2 -> {
+                    winner.setText("贏家\n平手")
+                    computer.setText("電腦出拳\n布")
+                }
+            }
+            mine.setText("玩家出拳\n布")
+        }
+    }
+
+
 }
